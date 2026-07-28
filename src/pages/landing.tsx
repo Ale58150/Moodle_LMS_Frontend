9aa27950bfd99528f943653ce4b17eea1ef97a08
Missa24 <missaelapaza@gmail.com>
2026-07-28 15:22:09 -0400

    se creo archivo landing

---
+++ src/pages/landing.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Especialidadseccion from
+ '@/features/page/sections/Especialidadseccion'
+const landing = () => {
+  return (
+    <div>
+      <Especialidadseccion />
+    </div>
+  )
+}
+
+export default landing
