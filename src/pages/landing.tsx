aafffd5d2556833558ae2af7d74c185e081f3ee4
Missa24 <missaelapaza@gmail.com>
2026-07-29 14:45:06 -0400

    se realizo la estrctura de fornt

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import Especialidadseccion from
  '@/features/page/sections/Especialidadseccion'
+ import Certificaciones from
+ '@/features/page/sections/Certificaciones'
+ import Docentes from '@/features/page/Docentes'  
+ import Testimonios from '@/features/page/sections/Testimonios'  
+ import PreguntasFrecuentes from '@/features/page/sections/PreguntasFrecuentes'
+ import Cursos from '@/features/page/sections/Cursos'
 const landing = () => {
   return (
     <div>
       <Especialidadseccion />
+      <Certificaciones />
+      <Docentes />    
+      <Testimonios />
+      <PreguntasFrecuentes />
+      <Cursos />
     </div>
   )
 }
