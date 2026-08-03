3ff7fff0d1293963169563d9535ea2f363c7af32
Missael Apaza <missaelapaza@gmail.com>
2026-08-03 08:58:02 -0400

    connect api Service from backend

---
+++ src/api/api.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+
+console.log(import.meta.env.VITE_API_URL)
+
+const apiService = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+    headers: {
+        "Content-Type": "application/json",
+    },
+
+});
+
+apiService.interceptors.request.use(config => {
+    const token = localStorage.getItem("token");
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
+
+export { apiService };
