749c7ef3a5ca0550b75d591b7c2e60fe931b6e22
Missael Apaza <missaelapaza@gmail.com>
2026-08-03 08:58:46 -0400

    components

---
+++ src/components/common/Apptittle.tsx
@@ -0,0 +1,18 @@
+interface AppTitleProps {
+    title: string;
+    subtitle: string;
+}
+
+export function AppTitle({ title, subtitle }: AppTitleProps) {
+    return (
+        <div>
+            <h1 className="text-lg font-bold tracking-wide text-primary">
+                {title}
+            </h1>
+
+            <p className="text-xs text-muted-foreground">
+                {subtitle}
+            </p>
+        </div>
+    );
+}
