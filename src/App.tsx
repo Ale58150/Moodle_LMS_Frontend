b03b3f4ca0802d6a0d036c5cbb0b971a12122fff
Missael Apaza <missaelapaza@gmail.com>
2026-08-18 14:48:16 -0400

    add ErrorBlanding and GlobalLoadingBar

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import { RouterProvider } from "react-router-dom"
 import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css";
 import { router } from "./routes/router";
+import { ErrorBoundary } from "@/components/common/app/ErrorBoundary";
+import { GlobalLoadingBar } from "@/components/common/app/GlobalLoadingBar";
 
 function App() {
   return (
-    <>
+    <ErrorBoundary>
       <div className="overflow-auto">
+        <GlobalLoadingBar />
         <RouterProvider router={router} />
         <ToastContainer />
       </div>
-    </>
+    </ErrorBoundary>
   )
 }
 
