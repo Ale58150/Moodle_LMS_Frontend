d0581453b108a2802b71172cddead1d28264e968
Missael Apaza <missaelapaza@gmail.com>
2026-08-18 14:59:29 -0400

    add FieldForm in Form

--- src/features/Auth/components/reset_password_form.tsx
+++ src/features/Auth/components/reset_password_form.tsx
@@ -1,21 +1,18 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import { ResetPasswordSchema, ResetPasswordSchemaType } from "../Schema/AuthSchema";
 import { useForm } from "react-hook-form";
+import { ResetPasswordSchema, ResetPasswordSchemaType } from "../Schema/AuthSchema";
 import { useChangePassword } from "../Hook/AuthHook";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import { Field, FieldGroup, FieldLabel } from "@/components/ui/field";
-import { Input } from "@/components/ui/input";
+import { Field, FieldGroup } from "@/components/ui/field";
 import { AppTitle } from "@/components/common/Apptittle";
+import { FormField } from "@/components/common/form/FormField";
 
 export function Reset_password_form() {
     const changePasswordMutation = useChangePassword();
-    const {
-        register,
-        handleSubmit,
-        formState: { errors }
-    } = useForm<ResetPasswordSchemaType>({
-        resolver: zodResolver(ResetPasswordSchema)
+
+    const { control, handleSubmit } = useForm<ResetPasswordSchemaType>({
+        resolver: zodResolver(ResetPasswordSchema),
     });
 
     const onSubmit = (data: ResetPasswordSchemaType) => {
@@ -23,63 +20,38 @@ export function Reset_password_form() {
     };
 
     return (
-        <div>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <Card>
-                    <CardHeader>
-                        <AppTitle title="Cambia tu contraseña" subtitle="Por favor, recuerda no olvidarte tu contraseña" />
-                    </CardHeader>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <Card>
+                <CardHeader>
+                    <AppTitle title="Cambia tu contraseña" subtitle="Por favor, recuerda no olvidarte tu contraseña" />
+                </CardHeader>
 
-                    <CardContent>
-                        <FieldGroup className="gap-3">
-                            <Field>
-                                <FieldLabel htmlFor="new_password">
-                                    Nueva contraseña
-                                </FieldLabel>
-                                <Input
-                                    id="new_password"
-                                    type="password"
-                                    placeholder="**********"
-                                    {...register("new_password")}
-                                />
-                                {errors.new_password && (
-                                    <span className="text-sm text-red-500">
-                                        {errors.new_password.message}
-                                    </span>
-                                )}
-                            </Field>
+                <CardContent>
+                    <FieldGroup className="gap-3">
+                        <FormField
+                            type="password"
+                            control={control}
+                            name="new_password"
+                            label="Nueva contraseña"
+                            placeholder="**********"
+                        />
 
-                            <Field>
-                                <FieldLabel htmlFor="confirm_password">
-                                    Confirmar contraseña
-                                </FieldLabel>
-                                <Input
-                                    id="confirm_password"
-                                    type="password"
-                                    placeholder="********"
-                                    {...register("confirm_password")}
-                                />
-                                {errors.confirm_password && (
-                                    <span className="text-sm text-red-500">
-                                        {errors.confirm_password.message}
-                                    </span>
-                                )}
-                            </Field>
+                        <FormField
+                            type="password"
+                            control={control}
+                            name="confirm_password"
+                            label="Confirmar contraseña"
+                            placeholder="********"
+                        />
 
-                            <Field>
-                                <Button
-                                    type="submit"
-                                    disabled={changePasswordMutation.isPending}
-                                >
-                                    {changePasswordMutation.isPending
-                                        ? "Cambiando..."
-                                        : "Cambiar contraseña"}
-                                </Button>
-                            </Field>
-                        </FieldGroup>
-                    </CardContent>
-                </Card>
-            </form>
-        </div>
+                        <Field>
+                            <Button type="submit" disabled={changePasswordMutation.isPending}>
+                                {changePasswordMutation.isPending ? "Cambiando..." : "Cambiar contraseña"}
+                            </Button>
+                        </Field>
+                    </FieldGroup>
+                </CardContent>
+            </Card>
+        </form>
     );
 }
